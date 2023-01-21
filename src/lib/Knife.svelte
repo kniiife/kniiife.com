a24a61cee1f4aaefd6455b9b0ce1086bd95e1e0a
Kyle Wagner <Keale2@gmail.com>
2023-01-21 15:45:39 -0500

    Make knives show up

---
+++ src/lib/Knife.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let data = {};
+</script>
+
+<h1>{data.name}</h1>
+<p>Brand: {data.brand}</p>
+<p>Blade Length: {data.bladeLength}</p>
+<p>type: {data.type}</p>
+<p>Description: {data.description}</p>
+<p>URL: <a href={data.url} target="_blank" rel="noreferrer">{data.url}</a></p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+	import { each } from 'svelte/internal';
+    import knives from '../../src/knives.json';
+    import Knife from '$lib/Knife.svelte'
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+{#each knives as knife}
+    <Knife data={knife} />
+{/each}
