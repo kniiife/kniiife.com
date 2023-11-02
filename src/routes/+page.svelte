ee447c9e79c0fc9b2e22be3d2c1d92563b55ea1c
Kyle Wagner <Keale2@gmail.com>
2023-11-01 23:21:22 -0400

    Add dialog component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
 	import { each } from 'svelte/internal';
 	import knives from '../../src/knives.json';
 	import Knife from '$lib/Knife.svelte';
+	import Modal from '../lib/Modal.svelte';
+
+	let showModal = false;
 </script>
 
 <header class="bg-stone-300 p-8 border-b-8">
@@ -15,12 +18,19 @@
 		<div class="text-center my-40">
 			<h1 class="text-8xl font-bold text-red-800">The Friendly Arms Race</h1>
 			<p class="text-3xl mt-4">A never-ending challenge where each blade aims to outdo the last. Join us on this thrilling journey of blade-length one-upmanship as we showcase our ever-growing collection of knives.</p>
+			<button on:click={() => (showModal = true)} class="inline-block py-2 px-8 bg-stone-100 hover:bg-white rounded mt-8 font-sans
+				font-bold">Show Rules</button>
 		</div>
 		{#each knives as knife}
 			<Knife data={knife} />
 		{/each}
 	</div>
 </main>
+
+<Modal bind:showModal>
+	<h2 slot="header">Rules</h2>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque recusandae aliquam non magni atque incidunt error fugit sequi labore veniam. Ipsa repudiandae numquam veritatis odio consectetur ducimus sit explicabo consequuntur!</p>
+</Modal>
 
 <footer class="bg-stone-300 py-1 mt-20">
     <div class="container mx-auto">
