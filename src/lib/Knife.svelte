da8e8823452e5e1f32f312864bc842b765c55c0a
Kyle Wagner <Keale2@gmail.com>
2023-11-01 22:13:22 -0400

    Update knife styles

--- src/lib/Knife.svelte
+++ src/lib/Knife.svelte
@@ -2,20 +2,25 @@
 	export let data = {};
 </script>
 
-<article class="mt-20">
-	<header>
-		<h1 class="text-8xl font-bold">{data.name}</h1>
-		<p class="text-2xl font-semibold italic ml-16">Brand: {data.brand}</p>
-	</header>
-	<div class="mt-4 ml-16">
-		<p><span class="font-bold">Blade Length:</span> {data.bladeLength} 🗡</p>
-		<p><span class="font-bold">type:</span> {data.type}</p>
-		<p><span class="font-bold">Description:</span> {data.description}</p>
-		<p>
-			<span class="font-bold">URL:</span>
-			<a href={data.url} target="_blank" rel="noreferrer" class="text-orange-600 font-bold"
-				>{data.url}</a
-			>
-		</p>
+<article class="mt-20 flex items-center rounded-3xl bg-stone-100 shadow-lg">
+	{#if data.images?.length > 0}
+		<img src={data.images[0]} alt="" class="max-w-lg rounded-l-3xl" />
+	{/if}
+	<div class="ml-36">
+		<header>
+			<h1 class="font-serif text-4xl font-bold">{data.name || '-'}</h1>
+			<p class="text-2xl font-semibold italic">Brand: {data.brand || '-'}</p>
+		</header>
+		<div class="mt-4">
+			<p><span class="font-bold">Blade Length:</span> {data.bladeLength || '-'} 🗡</p>
+			<p><span class="font-bold">type:</span> {data.type || '-'}</p>
+			<p><span class="font-bold">Description:</span> {data.description || '-'}</p>
+			<p>
+				<span class="font-bold">URL:</span>
+				<a href={data.url || '#'} target="_blank" rel="noreferrer" class="text-orange-600 font-bold"
+					>{data.url || '-'}</a
+				>
+			</p>
+		</div>
 	</div>
 </article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,16 @@
 
 <header class="bg-stone-300 p-8 border-b-8">
 	<div class="container mx-auto">
-		<h1 class="text-3xl font-black text-stone-800">KNIIIFE🗡</h1>
+		<h1 class="text-3xl font-black text-stone-800">KNIIIFE</h1>
 	</div>
 </header>
 
 <main>
 	<div class="container mx-auto">
+		<div class="text-center my-40">
+			<h1 class="text-8xl font-bold">The Friendly Arms Race</h1>
+			<p class="text-3xl mt-4">A never-ending challenge where each blade aims to outdo the last. Join us on this thrilling journey of blade-length one-upmanship as we showcase our ever-growing collection of knives.</p>
+		</div>
 		{#each knives as knife}
 			<Knife data={knife} />
 		{/each}
